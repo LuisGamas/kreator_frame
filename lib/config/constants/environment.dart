3d1da00040a2c473fe9c8d07f6fedefe81277c76
Luis Donaldo Gamas <luisgamas00@gmail.com>
2025-07-11 00:58:17 -0600

    feat: version update to be published

--- lib/config/constants/environment.dart
+++ lib/config/constants/environment.dart
@@ -16,7 +16,7 @@ class Environment {
 
   // * Variables of Dashboard Preference **DO NOT REMOVE**
   static const String dashName = 'Kreator Frame';
-  static const String dashVersion = '1.3.0';
+  static const String dashVersion = '1.4.0';
   static const String dashDeveloper = 'Luis Gamas';
 
   // * Variables for assets **DO NOT REMOVE**
